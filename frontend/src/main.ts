f1a686d955ba74c10eebaf68f2d5ad733383e735
Istok <51259559+Icetok@users.noreply.github.com>
2025-01-13 09:16:54 +0100

    Update to use pinia & check auth

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,23 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import { useUserStore } from '@/store/user';
+
 
 const app = createApp(App)
 
-app.use(router)
+// Create Pinia instance
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+
+// Check authentication state on app load
+const userStore = useUserStore();
+userStore.checkAuth();
 
 app.mount('#app')
