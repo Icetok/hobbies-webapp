00fcbcf7ebf7f45b3c9921a59d4ac07d78c0da87
Zahid <zahid011001@gmail.com>
2025-01-15 20:20:01 +0000

    admin to add custom users

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,27 @@
-
 from django.contrib import admin
+from .models import CustomUser, Hobby
 
-# Register your models here.
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+# Inline for the Hobby relationship in the User model
+class UserHobbyInline(admin.TabularInline):
+    model = CustomUser.hobbies.through  # Through model for the ManyToManyField
+    extra = 1  # Number of empty forms to show by default
 
-admin.site.register(CustomUser, UserAdmin)
+# Admin for the CustomUser model
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'name', 'date_of_birth', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email', 'name')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    inlines = [UserHobbyInline]  # Add inline for hobbies
+
+# Admin for the Hobby model
+class HobbyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+# Register the models
+admin.site.register(CustomUser, CustomUserAdmin)  # Register CustomUser with its custom admin
+admin.site.register(Hobby, HobbyAdmin)  # Register Hobby with its custom admin
+
+ # Username : admin
+ # Password: admin
+
