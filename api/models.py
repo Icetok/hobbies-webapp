5d3715eae073ff13d82473e326d7fc33ea972fb1
Zahid <zahid011001@gmail.com>
2025-01-15 19:31:33 +0000

    fixing database sqlite database issues and requirements.txt

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 class Hobby(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=101)
 
     def __str__(self):
         return self.name
