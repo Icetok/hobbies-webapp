c2653bb35fb9c2de7b5d09807e020f48374eb387
Istok <51259559+Icetok@users.noreply.github.com>
2025-01-17 10:43:24 +0100

    add friend requests page & check auth

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,6 +6,7 @@ import AddHobby from '../pages/AddHobby.vue';
 import SignupPage from '../pages/SignupPage.vue';
 import LoginPage from '../pages/LoginPage.vue';
 import SimilarUsersPage from '../pages/SimilarUsersPage.vue';
+import FriendRequestsPage from '../pages/FriendRequestsPage.vue';
 
 let base = (import.meta.env.MODE === 'development') ? import.meta.env.BASE_URL : '';
 
@@ -32,12 +33,23 @@ const router = createRouter({
       component: SimilarUsersPage,
       meta: { requiresAuth: true },
     },
+    {
+      path: '/friend-requests/',
+      name: 'Friend Requests',
+      component: FriendRequestsPage,
+      meta: { requiresAuth: true },
+    },
   ],
 });
 
 // Add a global navigation guard
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const userStore = useUserStore();
+
+  // Check authentication status if not already done
+  if (!userStore.isAuthenticated) {
+    await userStore.checkAuth();
+  }
 
   if (to.meta.requiresAuth && !userStore.isAuthenticated) {
     // If the route requires auth and the user is not logged in, redirect to Login
