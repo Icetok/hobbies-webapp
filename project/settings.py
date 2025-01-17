cb6fd40ad531dbc79599889fdcbd5086752153a0
Istok <51259559+Icetok@users.noreply.github.com>
2025-01-17 13:54:47 +0100

    static files

--- project/settings.py
+++ project/settings.py
@@ -145,8 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/stable/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Your custom static files directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/stable/ref/settings/#default-auto-field
